b72c3fa0b1d41dadfe4c9a8127fd10970f5ac61b
jessautogarage <jessautogarage@gmail.com>
2024-11-26 09:42:04 +0800

    Update web.php
    Routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function(){
+    echo "This is a Home Page!";
+});
+
+Route::get('/login', function() {
+    echo 'This is login page.';
+});
+
+Route::get('/create', function(){
+    echo 'This is create.';
+});
+
+Route::get('/read', function(){
+    echo 'This is read.';
+});
+
+Route::get('/update', function(){
+    echo 'This is update.';
+});
+
+Route::get('/delete', function(){
+    echo 'This is delete.';
+});
